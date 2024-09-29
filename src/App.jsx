45537b925ac80f24908520cc623e0329ebe24353
Kayla Atsila Ivanka <79374942+kaylaatsila@users.noreply.github.com>
2024-09-29 22:46:33 +0700

    feat: login UI and routes (#8)
    * feat: login UI
    
    * feat: updated login UI, simple route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
 import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from '@mantine/core';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import * as screens from './features';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <screens.LoginScreen />,
+  },
+  {
+    path: "login",
+    element: <screens.LoginScreen />,
+  },
+  {
+    path: "register",
+    element: <screens.RegisterScreen />,
+  },
+]);
+
+const theme = createTheme({
+  colors: {
+    'blue': [
+      '#F3F4F8',
+      '#E2E3F2',
+      '#8892E7',
+      '#5663DC',
+      '#2A3ACB',
+      '#1F2B98',
+      '#161f6d',
+      '#151D66',
+      '#111855',
+      '#0E1344'
+    ],
+
+    'light-blue': [
+      "#EBFAFF",
+      "#ADECFF",
+      "#70DEFF",
+      "#33CFFF",
+      "#1FCBFF",
+      "#00BCF5",
+      "#0097C6",
+      "#008DB8",
+      "#007DA3",
+      "#006D8F"
+    ]
+  }
+})
 
 export default function App() {
   return (
-    <MantineProvider >
-      <screens.RegisterScreen />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
     </MantineProvider>
   )
 }
